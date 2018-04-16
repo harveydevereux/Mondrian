5ae7db0ba2fdaa137f878d1734584b818f94aad5
Harvey Devereux <harveydevereux@googlemail.com>
2018-04-16 15:14:51 +0100

    finished mondrian process

--- Mondrian.jl
+++ Mondrian.jl
@@ -5,7 +5,13 @@ include("Axis_Aligned_Box.jl")
 
 function Mondrian_process(Θ::Axis_Aligned_Box, Tₗ = 5)
     cuts = []
-    return Mondrian_started_at(Θ::Axis_Aligned_Box,0, cuts, Tₗ), cuts
+    M = Mondrian_started_at(Θ::Axis_Aligned_Box,0, cuts, Tₗ)
+    a = []
+    for i in 1:length(cuts)
+        push!(a,cuts[i][4])
+    end
+    cuts = cuts[sortperm(a)]
+    return M,cuts
 end
 
 function Mondrian_started_at(Θ::Axis_Aligned_Box,t₀,cuts, Tₗ = 5)
@@ -32,6 +38,7 @@ function Mondrian_started_at(Θ::Axis_Aligned_Box,t₀,cuts, Tₗ = 5)
 end
 
 function show_mondrian_split_2d(split, times=false)
+    plot()
     for i in 1:size(split,1)
         d = split[i][2]
         int = split[i][3]
@@ -55,4 +62,30 @@ function show_mondrian_split_2d(split, times=false)
     title!("Mondrian Process splits")
     xlabel!("Box dimension 1")
     ylabel!("Box dimension 2")
+end
+
+function create_gif(split)
+    plot()
+    xlims!(split[1][3][1,1],split[1][3][1,2])
+    ylims!(split[1][3][2,1],split[1][3][2,2])
+    times=true
+    anim = @animate for i in 1:size(split,1)
+        d = split[i][2]
+        int = split[i][3]
+        if times
+            label = round(c[i][end],3)
+        else
+            label = ""
+        end
+        if (d == 1)
+            x = linspace(int[2,1],int[2,2],10)
+            plot!(fill(split[i][1],length(x)),x,show=true, label="")
+            annotate!([(split[i][1], median(x), text(label,6))])
+        else
+            x = linspace(int[1,1],int[1,2],10)
+            plot!(x,fill(split[i][1],length(x)),show=true, label="")
+            annotate!([(median(x),split[i][1], text(label,6))])
+        end
+    end
+    gif(anim,"mondrian.gif",fps=1)
 end
